349db18810b98b40978f75b958a4008aeb4e0acb
Udara J(SiniSTeR) <67888873+udaraJ08@users.noreply.github.com>
2022-12-20 15:27:59 +0530

    employee views init

---
+++ src/DB/DB.js
@@ -0,0 +1,6 @@
+export const DIVISION_MOCK_DB = [
+    {
+        name: 'DIV_001',
+        description: 'This is the first division'
+    }
+]
---
+++ src/custom-views/employeeView/table/tableHandler.js
@@ -0,0 +1,37 @@
+import { Edit, Trash2 } from "react-feather"
+
+export const employeeTableHandler = () => {
+
+    return  [
+        {
+            name: 'NAME',
+            sortable: true,
+            minWidth: '100px',
+            selector: row => {
+                return row?.name
+            }
+        },
+        {
+            name: 'DESCRIPTION',
+            sortable: true,
+            minWidth: '100px',
+            selector: row => {
+                return row.description
+            }
+        },
+        {
+            name: 'ACTIONS',
+            sortable: true,
+            minWidth: '100px',
+            selector: () => {
+                return <div>
+                    <button className='btn clickable'><Edit size={15}/></button>
+                    <button
+                        className='btn clickable'>
+                        <Trash2  size={15} color='crimson'/>
+                    </button>
+                </div>
+            }
+        }
+    ]
+}
