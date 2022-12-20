835d90f85c675dd7708e0f4dc9396ca9926a9a6c
Udara J(SiniSTeR) <67888873+udaraJ08@users.noreply.github.com>
2022-12-20 14:46:45 +0530

    divisions view done

--- src/custom-views/divisionsView/DivisionsView.js
+++ src/custom-views/divisionsView/DivisionsView.js
@@ -1,7 +1,37 @@
+import {Button, Card, CardBody, CardHeader, Col, Input, Label, Row} from "reactstrap"
+import DivisionManagementTable from "./table/DivisionManagementTable"
+
 const DivisionsView = () => {
 
     return <div>
-        <h1>This is the divisions !</h1>
+        <Card>
+            <CardHeader className='p-1 m-0 bg-gradient-primary font-large-1 f-Staatliches'>
+                Manage Divisions
+            </CardHeader>
+            <CardBody className='pt-2'>
+                <Row>
+                    <Col lg={4}>
+                        <Label htmlFor='divisionName' className='text-medium'>Division Name</Label>
+                        <Input id='divisionName' placeholder='Enter division name'/>
+                    </Col>
+                    <Col lg={8}>
+                        <Label htmlFor='divisionDescription' className='text-medium'>Description</Label>
+                        <Input id='divisionDescription' placeholder='Enter description'/>
+                    </Col>
+                </Row>
+                <div className='w-100 mt-2 d-flex justify-content-end'>
+                    <button className='btn btn-primary'>CREATE</button>
+                </div>
+            </CardBody>
+        </Card>
+        <Card className='mt-2'>
+            <CardHeader className='p-1 m-0 font-large-1 f-Staatliches'>
+                DIVISIONS
+            </CardHeader>
+            <CardBody>
+                <DivisionManagementTable />
+            </CardBody>
+        </Card>
     </div>
 }
 
