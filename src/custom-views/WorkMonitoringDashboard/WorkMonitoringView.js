a3e6569be149a8b893e55eebabf30a4d41352720
Udara J(SiniSTeR) <67888873+udaraJ08@users.noreply.github.com>
2022-12-29 07:32:05 +0530

    task create unit view done

--- src/custom-views/WorkMonitoringDashboard/WorkMonitoringView.js
+++ src/custom-views/WorkMonitoringDashboard/WorkMonitoringView.js
@@ -16,7 +16,7 @@ const WorkMonitoringView = () => {
 
     return <div>
         <h1 className='f-Staatliches font-large-1'>Work Monitoring Module</h1>
-        <p className='mt-3 text-danger'>*Select an unit to proceed</p>
+        <p className='mt-3 text-primary'>*Select an unit to proceed</p>
         <div className='d-flex gap-2 mt-2'>
             <Col onClick={() => routeToUnit(1)} lg={2}><UnitCard name='Unit 01'/></Col>
         </div>
--- src/custom-views/WorkMonitoringManageView/WorkMonitoringManageView.js
+++ src/custom-views/WorkMonitoringManageView/WorkMonitoringManageView.js
@@ -1,7 +1,25 @@
+import {useHistory} from "react-router-dom"
+import {Col} from "reactstrap"
+import UnitCard from "../../components/WorkMonitoringDashboard/UnitCard"
+
 const WorkMonitoringManageView = () => {
 
+    const history = useHistory()
+
+    const routeToUnit = (id) => {
+        history.push('/work-monitoring/unit-view', {
+            data: {
+                id
+            }
+        })
+    }
+
     return <div>
-        <h1>This is the work monitoring view !</h1>
+        <h1 className='f-Staatliches font-large-1'>Task Handle Module</h1>
+        <p className='mt-3 text-primary'>*Select an unit to proceed</p>
+        <div className='d-flex gap-2 mt-2'>
+            <Col onClick={() => routeToUnit(1)} lg={2}><UnitCard name='Unit 01'/></Col>
+        </div>
     </div>
 }
 
