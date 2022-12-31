a76fd3288d642540ad3280c4e62b06ad59f7ee7f
Udara J(SiniSTeR) <67888873+udaraJ08@users.noreply.github.com>
2022-12-31 10:21:28 +0530

    work monitoring name passing done

--- src/components/WorkMonitoringDashboard/UnitCard.js
+++ src/components/WorkMonitoringDashboard/UnitCard.js
@@ -1,10 +1,16 @@
-import {Card, CardBody} from "reactstrap"
+import {Card, CardBody, Col} from "reactstrap"
+import Avatar from "../../@core/components/avatar"
 
 const UnitCard = ({name}) => {
 
     return <Card className='clickable-mini'>
-        <CardBody className='d-center'>
-            <h5 className='p-0 m-0'>{name}</h5>
+        <CardBody className='d-flex'>
+            <Col lg={3}>
+                <Avatar img='https://media.istockphoto.com/id/1397556857/vector/avatar-13.jpg?b=1&s=170667a&w=0&k=20&c=1GR7K88AV_az799CO-ecA9bwoEKgHzuB-mtE0YBF00s='/>
+            </Col>
+            <Col className='d-flex align-items-center'>
+                <h5 className='p-0 m-0'>{name}</h5>
+            </Col>
         </CardBody>
     </Card>
 }
--- src/custom-views/WorkMonitoringUnitView/WorkMonitoringUnitView.js
+++ src/custom-views/WorkMonitoringUnitView/WorkMonitoringUnitView.js
@@ -1,10 +1,15 @@
 import {Card, CardBody, CardHeader, Col, Row} from "reactstrap"
 import WorkMonitoringCard from "../../components/WorkMonitoringUnitView/WorkMonitoringCard"
+import {useLocation} from "react-router-dom"
 
 const WorkMonitoringUnitView = () => {
 
+    const location = useLocation()
+
+    console.log(location.state.data)
+
     return <div>
-        <h1 className='f-Staatliches'>UNIT 01: Tasks list</h1>
+        <h1 className='f-Staatliches'>{location.state.data.name}'s: Tasks list</h1>
         <Row className='mt-2'>
             <Col lg={4}>
                 <Card className='bg-light-grey'>
